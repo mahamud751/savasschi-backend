db3fea797e56632cc78b4e8e5cbb8abe7086186c
root <root@srv1209731.hstgr.cloud>
2026-01-22 08:52:06 +0000

    update

--- src/content-management/content-management.module.ts
+++ src/content-management/content-management.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ContentManagementService } from './content-management.service';
 import { ContentManagementController } from './content-management.controller';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [ContentManagementController],
   providers: [ContentManagementService],
   exports: [ContentManagementService],
